fb4890448b7afccaf49b7c134e2fc87551e86ed2
Marco Paredes <marcop_93_04@hotmail.com>
2023-05-11 19:31:12 -0500

    delay

--- src/controllers/colorsController.ts
+++ src/controllers/colorsController.ts
@@ -4,11 +4,13 @@ import * as data from "../lib/Data.json";
 class ColorsController {
     public getColors(req: Request, res: Response) {
         const { type } = req.query;
-        if (type == '-1') return res.send(data);
-
-        const primario = type == '1';
-        const colors = data.arrayColores.filter(x => x.tipoPrimario == primario);
-        res.send(colors);
+        setTimeout(() => {
+            if (type == '-1') return res.send(data.arrayColores);
+    
+            const primario = type == '1';
+            const colors = data.arrayColores.filter(x => x.tipoPrimario == primario);
+            res.send(colors);    
+        }, 5000);
     }
 }
 
